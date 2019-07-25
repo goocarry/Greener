afd9c8135c7e5b53e72726cf6ef5fff7f677be81
Станислав Кузаков <goocarry@gmail.com>
2019-07-25 07:09:02 +0300

    until 1:35

--- screens/Login.js
+++ screens/Login.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
-import { KeyboardAvoidingView, View, StyleSheet } from 'react-native';
+import { ActivityIndicator, Keyboard, KeyboardAvoidingView, View, StyleSheet } from 'react-native';
 
 import { Block, Button, Text, Input } from '../components';
 
 import { theme } from '../constants';
 
+const VALID_EMAIL = "goocarry@gmail.com";
+const VALID_PASSWORD = "123456";
+
+
 export default class Login extends Component {
 
   state = {
-    email: "goocarry@gmail.com",
-    password: "123456",
+    email: '',
+    password: '',
+    errors: [],
+    loading: false,
+  }
+
+  handleLogin() {
+    const { navigation } = this.props;
+    const { email, password } = this.state;
+    const errors = [];
+
+    Keyboard.dismiss();
+
+    this.setState({ loading: true });
+
+    //check with backend API or with some static data
+    if (email !== VALID_EMAIL) {
+      errors.push('email');
+    }
+    if (password !== VALID_PASSWORD) {
+      errors.push('password');
+    }
+
+    this.setState({ errors, loading: false });
+
+    if (!errors.length) {
+      navigation.navigate("Browse");
+    }
   }
 
   render() {
+    const { navigation } = this.props;
+    const { loading, errors } = this.state;
+    const hasErrors = key => errors.includes(key) ? styles.hasErrors : null;
     return (
       <KeyboardAvoidingView style={styles.login} behavior="padding">
         <Block padding={[0, theme.sizes.padding * 2]}>
@@ -20,17 +53,28 @@ export default class Login extends Component {
           <Block middle>
             <Input
               label="Email"
-              style={styles.input}
+              style={[styles.input, hasErrors('email')]}
               defaultValue={this.state.email}
               onChangeText={text => this.setState({ email: text })}
             ></Input>
             <Input
               secure
               label="Password"
-              style={styles.input}
+              style={[styles.input, hasErrors('password')]}
               defaultValue={this.state.password}
               onChangeText={text => this.setState({ password: text })}
             ></Input>
+            <Button gradient onPress={() => this.handleLogin()}>
+              {loading ? <ActivityIndicator size="small" color="white" /> :
+                <Text bold white center>Login</Text>
+              }
+
+            </Button>
+
+            <Button onPress={() => { }}>
+              <Text gray caption center style={{ textDecorationLine: 'underline' }}>Forgot your password?</Text>
+            </Button>
+
           </Block>
         </Block>
       </KeyboardAvoidingView>
@@ -49,5 +93,8 @@ const styles = StyleSheet.create({
   login: {
     flex: 1,
     justifyContent: 'center',
+  },
+  hasErrors: {
+    borderBottomColor: theme.colors.accent,
   }
 })
