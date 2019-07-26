0b44ae6fe4ac5b09d2bb4e41f99bf4d7eb7a8417
goocarry <goocarry@gmail.com>
2019-07-26 15:47:02 +0900

    until 2:40

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, ScrollView } from 'react-native';
+import Slider from 'react-native-slider';
 
-import { Block, Button, Text, Card, Badge } from '../components';
+import { Block, Button, Text, Divider } from '../components';
 import { theme, mocks } from '../constants';
-import { ScrollView } from 'react-native-gesture-handler';
 
 class Settings extends Component {
+  state = {
+    budget: 850,
+    monthly: 1700,
+  }
   render() {
     const { profile, navigation } = this.props;
 
@@ -22,7 +26,7 @@ class Settings extends Component {
         </Block>
         <ScrollView showsVerticalScrollIndicator={false}>
           <Block style={styles.inputs}>
-            <Block row space="between" margin={[0, 0]} style={styles.inputRow}>
+            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
                 <Text gray2 style={{ marginBottom: 10 }}>Username</Text>
                 <Text bold>goocarry</Text>
@@ -31,7 +35,7 @@ class Settings extends Component {
                 Edit
               </Text>
             </Block>
-            <Block row space="between" margin={[0, 0]} style={styles.inputRow}>
+            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
                 <Text gray2 style={{ marginBottom: 10 }}>Location</Text>
                 <Text bold>Yakutsk</Text>
@@ -40,7 +44,7 @@ class Settings extends Component {
                 Edit
               </Text>
             </Block>
-            <Block row space="between" margin={[0, 0]} style={styles.inputRow}>
+            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
                 <Text gray2 style={{ marginBottom: 10 }}>E-mail</Text>
                 <Text bold>goocarry@gmail.com</Text>
@@ -50,6 +54,45 @@ class Settings extends Component {
               </Text>
             </Block>
           </Block>
+
+          <Divider />
+
+          <Block style={styles.sliders}>
+            <Block>
+              <Text gray2>Budget</Text>
+              <Slider
+                minimumValue={0}
+                maximumValue={1000}
+                style={{ height: 10 }}
+                thumbStyle={styles.thumb}
+                trackStyle={{ height: 6, borderRadius: 6 }}
+                minimumTrackTintColor={theme.colors.secondary}
+                maximumTrackTintColor="rgba(157,163,180,0.10"
+                value={this.state.budget}
+                onValueChange={value => this.setState({ budget: value })}
+              />
+              <Text caption gray2 right>$1,000</Text>
+            </Block>
+
+            <Block>
+              <Text gray2>Monthly Cap</Text>
+              <Slider
+                minimumValue={0}
+                maximumValue={1000}
+                style={{ height: 10 }}
+                thumbStyle={styles.thumb}
+                trackStyle={{ height: 6, borderRadius: 6 }}
+                minimumTrackTintColor={theme.colors.secondary}
+                maximumTrackTintColor="rgba(157,163,180,0.10"
+                value={this.state.monthly}
+                onValueChange={value => this.setState({ monthly: value })}
+              />
+              <Text caption gray2 right>$5,000</Text>
+            </Block>
+          </Block>
+
+          <Divider />
+
         </ScrollView>
       </Block>
     );
@@ -71,9 +114,22 @@ const styles = StyleSheet.create({
     width: theme.sizes.base * 2.2,
   },
   inputs: {
+    marginTop: theme.sizes.base * 0.7,
     paddingHorizontal: theme.sizes.base * 2,
   },
   inputRow: {
     alignItems: 'flex-end'
+  },
+  sliders: {
+    marginTop: theme.sizes.base * 0.7,
+    paddingHorizontal: theme.sizes.base * 2,
+  },
+  thumb: {
+    width: theme.sizes.base,
+    height: theme.sizes.base,
+    borderRadius: theme.sizes.base,
+    borderColor: 'white',
+    borderWidth: 3,
+    backgroundColor: theme.colors.secondary,
   }
 })
